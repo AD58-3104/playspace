0a43ade37d2fa5c6019e64015dbe2acd665c186d
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-04-30 01:30:07 +0900

    asdfsadf

--- socket/receiver.cpp
+++ socket/receiver.cpp
@@ -9,14 +9,14 @@ using namespace chrono_literals;
 int main(int argc, const char **argv)
 {
     webotsvision::CameraMeasurement recv_pic;
-    constexpr int32_t max_len = 307214;
+    static const int32_t max_len_message = 800*480;
     std::string received_data;
     //received_data.resize(max_len);
     uint64_t recv_size = 0;
     unsigned int priority = 0;
-    received_data.resize(max_len);
+    received_data.resize(max_len_message);
     boost::interprocess::message_queue::remove("WEBOTS_PICTURE_COMMUNICATION");
-    boost::interprocess::message_queue msgq(boost::interprocess::create_only, "WEBOTS_PICTURE_COMMUNICATION", 5, max_len);
+    boost::interprocess::message_queue msgq(boost::interprocess::create_only, "WEBOTS_PICTURE_COMMUNICATION", 5, max_len_message);
     while (1)
     {
         msgq.receive(&received_data[0], received_data.size(), recv_size, priority);
--- socket/sender.cpp
+++ socket/sender.cpp
@@ -3,15 +3,44 @@
 #include <thread>
 #include <cstring>
 #include "picture.pb.h"
+#include <chrono>
 using namespace std;
 using namespace chrono_literals;
-
-
-int main(int argc, const char **argv)
+class qu
 {
-    webotsvision::CameraMeasurement picture,t;
-    constexpr int size = 640*480;
-    std::string image(size,'0');
+    public:
+    bool f;
+    boost::interprocess::message_queue q;
+    qu():f(wait()),q(boost::interprocess::open_only, "WEBOTS_PICTURE_COMMUNICATION"){
+
+    }
+    bool wait()
+    {
+        while (1)
+        {
+            try
+            {
+                boost::interprocess::message_queue wait(boost::interprocess::open_only, "WEBOTS_PICTURE_COMMUNICATION");
+            }
+            catch (boost::interprocess::interprocess_exception ex)
+            {
+                std::cerr << "not exist" << std::endl;
+                std::this_thread::sleep_for(1000ms);
+                continue;
+            }
+            std::cout << "arimasita!" << std::endl;
+            break;
+        }
+        return true;
+    }
+};
+
+int
+main(int argc, const char **argv)
+{
+    webotsvision::CameraMeasurement picture, t;
+    constexpr int size = 640 * 480;
+    std::string image(size, '0');
     std::string send_data;
     picture.set_image(image);
     picture.set_simtime(765765);
@@ -21,10 +50,32 @@ int main(int argc, const char **argv)
     //t.ParseFromString(send_data);
     //cout << t.DebugString();
     //cout << t.simtime() << endl;
+
+
+
+    qu aa;
+    std::cout << "end";
+    return 0;
     {
+        while (1)
+        {
+            try
+            {
+                boost::interprocess::message_queue wait(boost::interprocess::open_only, "WEBOTS_PICTURE_COMMUNICATION");
+            }
+            catch (boost::interprocess::interprocess_exception ex)
+            {
+                std::cerr << "not exist" << std::endl;
+                std::this_thread::sleep_for(1000ms);
+                continue;
+            }
+            std::cout << "arimasita!" << std::endl;
+            break;
+        }
         boost::interprocess::message_queue msgq(boost::interprocess::open_only, "WEBOTS_PICTURE_COMMUNICATION");
 
-        for (int i = 0; i < 10; ++i){
+        for (int i = 0; i < 10; ++i)
+        {
             msgq.send(&send_data[0], send_data.size(), 0);
             std::this_thread::sleep_for(1000ms);
             std::cout << send_data.size() << std::endl;
