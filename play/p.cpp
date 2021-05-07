a67da72233a08f8f79d4b7426c0a6e0ce2bbae0e
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-05-07 13:07:56 +0900

    asd

---
+++ play/p.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include <bitset>
+using namespace std;
+int main(int argc, const char** argv) {
+    string a = ("井上叡");
+    //u32string b;
+    cin >> a;
+    cout << a;
+    
+    return 0;
+}
