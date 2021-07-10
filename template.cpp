59c3c61a10bb4b57323449236ad567fba64b868e
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-07-10 22:39:21 +0900

    asdofijsiodfjioasjdfiojsa

---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+using namespace std;
+using namespace std::literals::chrono_literals;
+
+int main(int argc, const char** argv) {
+    return 0;
+}
