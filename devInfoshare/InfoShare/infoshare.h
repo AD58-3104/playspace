1f38195381684232585584ca5ade879d8d4432b2
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-07-31 16:29:16 +0900

    asdfoijasdfoijawefoijwaefoijweafoij

--- devInfoshare/InfoShare/infoshare.h
+++ devInfoshare/InfoShare/infoshare.h
@@ -13,10 +13,19 @@ namespace Citbrains
     {
     public:
         [[nodiscard]] getter(const int id )const noexcept; 
+        void terminate(void);
+        bool sendInfomationToOtherRobots();
+        void changeColor(const int id, const int color);
+		void setTimeFunc(float (*func)());
+		float getTime(void);
 
     private:
         std::vector<std::unique_ptr<Citbrains::otherRobotInfomation>> robot_data_list_;
-        MessageProto::SharingData sharing_data;
+        MessageProto::SharingData sharing_data_;
+        void receiveSharedInfomation();
+
+        void receivingThreadLoop();
+        void sendingThreadLoop();
     };
 
 }
