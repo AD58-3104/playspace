e85de9d933b9a321d80c8d1bd33194bb95853b13
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-09-10 23:37:16 +0900

    asdfoijasd

--- devInfoshare/InfoShare/infoshare.cpp
+++ devInfoshare/InfoShare/infoshare.cpp
@@ -1,22 +1,17 @@
 #include "infoshare.h"
 #include <array>
 #include <boost/asio.hpp>
+#include <ctime>
 using namespace std::literals::chrono_literals;
 namespace Citbrains
 {
     namespace infosharemodule
     {
-
-        InfoShare::InfoShare(const int32_t self_id, const int32_t our_color, const int32_t number_of_our_robots, const std::string ip_address, float (*timeFunc)())
-            : self_id_(self_id), terminated_(false), our_color_(our_color), number_of_our_robots_(number_of_our_robots)
+        //デフォルトの時間関数を使いたい場合は引数無し
+        //必ずsetupを呼ぶ。
+        InfoShare::InfoShare()
+            : self_id_(1), our_color_(COLOR_MAGENTA), ip_adress_("127.0.0.1"), timeFunc_(timefunc), terminated_(false)
         {
-            for (int32_t i = 0; i < number_of_our_robots_; ++i)
-            {
-                robot_data_list_.push_back(std::make_unique<Citbrains::infosharemodule::OtherRobotInfomation>(i,timeFunc));
-            }
-            receivedDataHandler_ = [&](std::string && data){ //全てキャプチャするのは嫌だが取り敢えずこうしておく。参照の寿命とラムダの寿命は同じ。
-                //パースして中に入れる。大分面倒なので後で.....。
-            };
         }
         InfoShare::~InfoShare()
         {
@@ -53,11 +48,53 @@ namespace Citbrains
         //         }
         //     }
         // }
-
         void InfoShare::terminate()
         {
             //clientとserverのio_serviceをstopする為のterminateを呼ぶ。
             terminated_ = true;
+        }
+        void InfoShare::changeColor(const int32_t color)
+        {
+            our_color_ = color;
+        }
+        void InfoShare::setTimeFunc(float (*func)())
+        {
+            timeFunc_ = func;
+        }
+        //既にtimefuncを設定済みの場合は渡さなければ変更されない。
+        void InfoShare::setup(const int32_t self_id = 1, const int32_t our_color = COLOR_MAGENTA, const std::string ip_adress = "127.0.0.1", float (*timefunc)() = nullptr)
+        {
+            self_id_ = self_id;
+            ip_adress_ = ip_adress;
+            our_color_ = our_color;
+            if (timefunc != nullptr)
+            {
+                timeFunc_ = timefunc;
+            }
+            for (int32_t i = 0; i < NUM_PLAYERS; ++i)
+            {
+                robot_data_list_.push_back(std::make_unique<Citbrains::infosharemodule::OtherRobotInfomation>(i + 1, timeFunc_));
+            }
+            receivedDataHandler_ = [&](std::string &&data) { //全てキャプチャするのは嫌だが取り敢えずこうしておく。参照の寿命とラムダの寿命は同じ。
+                //パースして中に入れる。大分面倒なので後で.....。
+                std::string s(std::move(data));
+                CitbrainsMessage::SharingData shared_data;
+                shared_data.ParseFromString(s);
+                //ここで必ずsetRecv_timeを呼びます。
+            };
+        }
+        float InfoShare::getTime()
+        {
+            if (timeFunc_ != nullptr)
+            {
+                return timeFunc_();
+            }
+            return (float)time(0);
+        }
+
+        int32_t InfoShare::getOurcolor()
+        {
+            return our_color_;
         }
     }
 }
--- devInfoshare/InfoShare/infoshare.h
+++ devInfoshare/InfoShare/infoshare.h
@@ -16,135 +16,144 @@ namespace Citbrains
 {
     namespace infosharemodule
     {
-        /*
+/*
+        TODO!!!!
         commandstring 
         behavierstringをここにハードコーディングする
         
         
-
         */
+//inli{ne変数を使っているのでc++17以上でしかコンパイル出来ない。
+#ifdef __cpp_inline_variables
+        // static_assert(false,"this environment has inline variables");確認用
+        inline static constexpr int32_t NUM_PLAYERS = 4;
+        inline static constexpr int32_t COMM_INFO_PORT0 = 7110; //!< CommInfoで使用するPORTのはじめのポート
+                                                                //1:7110, 2:7111, 3:7112, 4:7113, 5:7114, 6:7115
 
+        inline static constexpr int32_t MAX_COMM_INFO_OBJ = 7; //!< 共有するオブジェクトの最大値(はじめはボール)
+        inline static constexpr int32_t MAX_STRING = 42;       //!< メッセージの最大値
+        inline static constexpr int32_t MAX_BEHAVIOR_STRING = 32;
 
+        inline static constexpr uint8_t COMM_NOT_EXIST = 0b0000'0000;     //!< 発見しないときの値　もう必要なさそうhasを使えば良いし
+        inline static constexpr uint8_t COMM_EXIST = 0b0000'0001;         //!< 発見しかたどうかのフラグ
+        inline static constexpr uint8_t COMM_OUR_SIDE = 0b0000'0010;      //!< 自チームかどうかのフラグ
+        inline static constexpr uint8_t COMM_OPPOSITE_SIDE = 0b0000'0100; //!< 敵チームかどうかのフラグ
 
+        inline static constexpr int32_t MAX_BLACK_POLES = 6; //!< 黒ポールの数
+        inline static constexpr int32_t MAX_TARGET_POS = 1;
+        inline static constexpr int32_t MAX_MAGENTA_OBJECTS = 3;                //!< マゼンタロボットの数
+        inline static constexpr int32_t MAX_CYAN_OBJECTS = MAX_MAGENTA_OBJECTS; //!< シアンロボットの数
 
-
-
+        inline static constexpr int32_t NO_MAGENTA = 0; //!< マゼンタの番号(色の番号とは異なる)
+        inline static constexpr int32_t NO_CYAN = 1;    //!< シアンの番号
+#else
+        //jetsonで無理だったら実装する.書くと長すぎて見にくいので.
+        static_assert(false, "this environment doesn't have inline variables");
+#endif 
         struct OtherRobotInfomation
         {
-            enum class MutexTag:int32_t{
+            enum class MutexTag : int32_t
+            {
                 OUR_ROBOT_GL,
                 ENEMY_ROBOT_GL,
                 BLACK_POLE_GL,
                 TARGET_POS_VEC,
                 RECV_TIME,
-                LENGTH  //最大数を知りたい時に使うので何か追加する時はこれの前に追加する。
+                LENGTH //最大数を知りたい時に使うので何か追加する時はこれの前に追加する。
             };
             //scoped_lockして
-            OtherRobotInfomation(int32_t id,float (*timeFunc)()):id_(id),timeFunc_(timeFunc){
-                for(int32_t i = 0;i < static_cast<int32_t>(MutexTag::LENGTH);++i){
+            OtherRobotInfomation(int32_t id, float (*timeFunc)()) : id_(id), timeFunc_(timeFunc)
+            {
+                for (int32_t i = 0; i < static_cast<int32_t>(MutexTag::LENGTH); ++i)
+                {
                     dataMutexes_.emplace_back();
                 }
             }
-            const int32_t id_;      //idは0スタートで管理する
-            float (*timeFunc_)();
+            void setTimeFunc(float (*timeFunc)())
+            {
+                timeFunc_ = timeFunc;
+            }
 
+            void setRecv_time(void)
+            {
+                std::lock_guard lock(dataMutexes_[static_cast<int32_t>(MutexTag::RECV_TIME)]);
+                if (timeFunc_ != nullptr)
+                {
+                    recv_time_ =  timeFunc_();
+                }
+                recv_time_ = (float)time(0);
+            }
+
+            const int32_t id_; //idは0スタートで管理する
+            float (*timeFunc_)();
+            std::deque<std::mutex> dataMutexes_; //vectorだとmutexのコピーコンストラクタを呼んでしまうのでdeque
+
+            //------------atomic datas----------------
             std::atomic_uint32_t cf_own_ = 0;
-            std::atomic_uint32_t cf_ball_ = 0;                  //fastにした方が良いのかもしれない
-            std::atomic_uint32_t status_ = 0;           //なんかこれフラグの詰め合わせっぽいので分けてatomic_boolで持つべき。
+            std::atomic_uint32_t cf_ball_ = 0; //fastにした方が良いのかもしれない
+            std::atomic_uint32_t status_ = 0;  //なんかこれフラグの詰め合わせっぽいので分けてatomic_boolで持つべき。
             std::atomic_uint32_t fps_ = 0;
-            std::atomic_uint32_t voltage_ = 0;  //使ってない。
-            std::atomic_uint32_t temperature_ =0;
+            std::atomic_uint32_t voltage_ = 0; //使ってない。
+            std::atomic_uint32_t temperature_ = 0;
             std::atomic_uint32_t highest_servo_ = 0;
             std::atomic_bool is_detect_ball_ = false;
             std::atomic_uint32_t strategy_no_ = 0;
-            std::atomic_uint32_t command_ = 0;              //文字列は長いので数字で管理する    returnで返すdataはどっかで文字列に変換して返すべき
-            std::atomic_uint32_t current_behavior_name_ = 0;//上に同じく
-
-            float recv_time_ = 0.0;      //c++20からですが......
-            std::vector<Pos2D> our_robot_gl_;  //扱いが良く分からんやつら
+            std::atomic_uint32_t command_ = 0;               //文字列は長いので数字で管理する    returnで返すdataはどっかで文字列に変換して返すべき
+            std::atomic_uint32_t current_behavior_name_ = 0; //上に同じく
+            //-------------non atomic datas---------------
+            float recv_time_ = 0.0;           //c++20からですが......
+            std::vector<Pos2D> our_robot_gl_; //扱いが良く分からんやつら
             std::vector<Pos2D> enemy_robot_gl_;
             std::vector<Pos2D> black_pole_gl_;
             std::vector<Pos2D> target_pos_vec_;
-
-
-            std::deque<std::mutex> dataMutexes_; //vectorだとmutexのコピーコンストラクタを呼んでしまうのでdeque
         };
 
         class InfoShare
         {
         public:
-            //inline変数を使っているのでc++17以上でしかコンパイル出来ない。
-            #ifdef  __cpp_inline_variables
-            // static_assert(false,"this environment has inline variables");確認用
-            inline static constexpr int32_t NUM_PLAYERS = 4;
-            inline static constexpr int32_t COMM_INFO_PORT0 = 7110; //!< CommInfoで使用するPORTのはじめのポート
-                                                                    //1:7110, 2:7111, 3:7112, 4:7113, 5:7114, 6:7115
-
-            inline static constexpr int32_t MAX_COMM_INFO_OBJ = 7;    //!< 共有するオブジェクトの最大値(はじめはボール)
-            inline static constexpr int32_t MAX_STRING = 42;          //!< メッセージの最大値
-            inline static constexpr int32_t MAX_BEHAVIOR_STRING = 32; 
-
-            inline static constexpr uint8_t COMM_NOT_EXIST = 0b0000'0000;     //!< 発見しないときの値　もう必要なさそうhasを使えば良いし
-            inline static constexpr uint8_t COMM_EXIST = 0b0000'0001;         //!< 発見しかたどうかのフラグ
-            inline static constexpr uint8_t COMM_OUR_SIDE = 0b0000'0010;      //!< 自チームかどうかのフラグ
-            inline static constexpr uint8_t COMM_OPPOSITE_SIDE = 0b0000'0100; //!< 敵チームかどうかのフラグ
-
-            inline static constexpr int32_t MAX_BLACK_POLES = 6; //!< 黒ポールの数
-            inline static constexpr int32_t MAX_TARGET_POS = 1;
-            inline static constexpr int32_t MAX_MAGENTA_OBJECTS = 3;                //!< マゼンタロボットの数
-            inline static constexpr int32_t MAX_CYAN_OBJECTS = MAX_MAGENTA_OBJECTS; //!< シアンロボットの数
-
-            inline static constexpr int32_t NO_MAGENTA = 0;    //!< マゼンタの番号(色の番号とは異なる)
-            inline static constexpr int32_t NO_CYAN = 1;       //!< シアンの番号
-            #else
-            //jetsonで無理だったら実装する.書くと長すぎて見にくいので.
-            static_assert(false,"this environment has not inline variables");
-            #endif
-            InfoShare(const int32_t self_id,const int32_t our_color,const int32_t number_of_our_robots,const std::string ip_address,float (*timeFunc)() );
-            InfoShare( const InfoShare & ) = delete ;
-            InfoShare & operator = ( const InfoShare & ) = delete ;
+            InfoShare();
+            InfoShare(const InfoShare &) = delete;
+            InfoShare &operator=(const InfoShare &) = delete;
             ~InfoShare();
 
-            //---------------------------------------------------------------------
-            //atomicなloadをしたのを返す一連のgetter。
+            //-----------------------other robot data getter----------------------
             //自分のidを指定された場合0に相当するものを返す。
-            [[nodiscard]] int32_t get_cf_own(const int32_t& id) const noexcept; 
-            [[nodiscard]] std::string get_command(const int32_t& id) const ; //残念ながらscoped_lockは例外を投げるらしい
-            //---------------------------------------------------------------------
-            
-            void terminate(void);
-            // bool sendInfomationToOtherRobots();
+            [[nodiscard]] int32_t getCf_own(const int32_t &id) const noexcept;
+            [[nodiscard]] std::string getCommand(const int32_t &id) const; //残念ながらscoped_lockは例外を投げるらしい
+            [[nodiscard]] float getRecv_time(const int32_t &id) const;     //残念ながらscoped_lockは例外を投げるらしい
+            //--------------------------------------------------------------------
+
+            void terminate();
             void changeColor(const int32_t color);
             void setTimeFunc(float (*func)());
-            float getTime(void);
+            void setup(const int32_t self_id, const int32_t our_color, const std::string ip_adress, float (*func)());
+            float getTime(); //getelapsedtimeとかの方が良いかも
+            int32_t getOurcolor();
+            int32_t getID();
             //TODO:名前変える
-            int32_t sendCommonInfo/*setSharingDatasAndSendInfomationToOtherRobotsにしたい*/(); //パラメータパックで受け取っても良いが、使われる場所がhplの一か所のみなので寧ろそうしない方が良さそう。
+            int32_t sendCommonInfo /* setSharingDataAndSendInfomationにしたい */ (); //パラメータパックで受け取っても良いが、使われる場所がhplの一か所のみなので寧ろそうしない方が良さそう。
 
         private:
-            const int32_t self_id_;
-            bool terminated_;
+            int32_t self_id_;
             int32_t our_color_;
-            const int32_t number_of_our_robots_;
+            std::string ip_adress_;
+            float (*timeFunc_)();
+            bool terminated_;
+
             std::vector<std::unique_ptr<Citbrains::infosharemodule::OtherRobotInfomation>> robot_data_list_;
             CitbrainsMessage::SharingData sharing_data_;
-            std::function<void(std::string &&)> receivedDataHandler_ ;
-            // void receiveSharedInfomation(); serverクラスに仕事無くされた
-
+            std::function<void(std::string &&)> receivedDataHandler_;
             //server-----------------------------------------------
             //多分2つ共クラスを持つ方が良い。
-            //client----------------------------------------------- 
-
+            //client-----------------------------------------------
         };
 
     }
 }
 
-
-
-
-
 /*
  * memo
+ * IDは内部での扱いは0スタート、インターフェースでは1スタートとする。
  * hplから使っている必要なインターフェース
  * setup().多分IDと色とアドレスを設定する
  * terminate()普通にterminate
