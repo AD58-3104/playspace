e392c0d96e279c132d03583ffedca15b46ffeaa5
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-11-13 15:12:15 +0900

    tabun kansei

--- python/scripts/audio_play.py
+++ python/scripts/audio_play.py
@@ -1,28 +1,13 @@
 import os
 
 def sound_play(sound_number:int):
-    sound_name_list:str = [
-    "001.wav",
-    "002.wav",
-    "003.wav",
-    "004.wav",
-    "005.wav",
-    "006.wav",
-    "007.wav",
-    "008.wav",
-    "ball_lost.wav",
-    "douzo_douzo.wav",
-    "ikkuzoo.wav",
-    "initialized.wav",
-    "localization.wav",
-    "lowvoltage.wav",
-    "ready.wav",
-    "settingfailed.wav",
-    "shoot.wav"]
+    dirname:str = "./sound"
+    names = os.listdir(dirname)
+    names.sort()
     aplay:str = 'aplay '
     echo:str = 'echo '
     if sound_number < len(sound_name_list):
-        os.system(echo+sound_name_list[sound_number])
+        os.system(echo+names[sound_number])
     else :
         print('there is no sound which match inputed number')
 
