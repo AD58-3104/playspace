f5bdcf38c77adaedf12dc5308926e12c822b8f34
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-11-27 18:41:09 +0900

    fix

--- python/scripts/parse_htn_name.py
+++ python/scripts/parse_htn_name.py
@@ -1,7 +1,7 @@
 import sys
 import os 
 
-def parseAndWriteDictionaryFile(readfile_fd,writefile_fd):
+def parseAndWriteDictionaryFile(readfile_fd,word_list):
     lines = readfile_fd.readlines()
     for line in lines:
         if "class" in line:
@@ -9,16 +9,32 @@ def parseAndWriteDictionaryFile(readfile_fd,writefile_fd):
             line = line.replace('\t','')
             line = line.split(" ")[1]
             line = line.split("(")[0]
-            writefile_fd.write(line)
-            print(line)
-    readfile.close()
-    writefile.close()    
+            word_list.add(line)
+    readfile_fd.close()
+    return word_list
 
-readfile = open('common_network.py', 'r')
-writefile = open('behavior_name_dict.txt','w')
-parseAndWriteDictionaryFile(readfile,writefile)
+def main(path_to_kid_dir):
+    behavior_name_list = set()
+    readfile = open(path_to_kid_dir + 'common_network.py', 'r')
+    behavior_name_list = parseAndWriteDictionaryFile(readfile,behavior_name_list)
+    readfile = open(path_to_kid_dir + 'root_network.py','r')
+    behavior_name_list = parseAndWriteDictionaryFile(readfile,behavior_name_list)
+    writefile = open('behavior_name_dict.dat','w')
+    for word in behavior_name_list:
+        writefile.write(word + '\n')
+    print('--------------------------------------------------')
+    commnad_name_list = set()
+    readfile = open(path_to_kid_dir + 'goal.py','r')
+    commnad_name_list = parseAndWriteDictionaryFile(readfile,commnad_name_list)
+    readfile = open(path_to_kid_dir + 'role.py','r')
+    commnad_name_list = parseAndWriteDictionaryFile(readfile,commnad_name_list)
+    readfile = open(path_to_kid_dir + 'tactics.py','r')
+    commnad_name_list = parseAndWriteDictionaryFile(readfile,commnad_name_list)
+    writefile = open('command_dict.dat','w')
+    for word in commnad_name_list:
+        writefile.write(word + '\n')
 
-print('--------------------------------------------------')
-readfile = open('root_network.py')
-writefile = open('behavior_name_dict.txt','a')
-parseAndWriteDictionaryFile(readfile,writefile)
+
+if __name__ == '__main__': 
+    
+    main(sys.argv[1])
