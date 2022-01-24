523cdb6ed80d38e0bbbf95e10bf5505777357d7a
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2022-01-25 00:41:41 +0900

    aaaaa

--- my_project/zeromq/a.cpp
+++ my_project/zeromq/a.cpp
@@ -12,27 +12,42 @@
 void Pub(zmq::context_t &ctx)
 {
     zmq::socket_t publisher(ctx, zmq::socket_type::pub);
-    publisher.bind("ipc://test_desu");
+    publisher.bind("ipc:///tmp/hajime_quaternion");
     int i = 0;
     while (true)
     {
         //  Write three messages, each with an envelope and content
-        publisher.send(zmq::str_buffer("Message A::"), zmq::send_flags::sndmore);
-        publisher.send(zmq::buffer(std::to_string(i)));
-        std::this_thread::sleep_for(std::chrono::milliseconds(700));
+        // publisher.send(zmq::str_buffer("Message A::"), zmq::send_flags::sndmore);
+        publisher.send(zmq::buffer("Pub1!!!!!!!!! message::" + std::to_string(i)));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
         i++;
     }
 }
 
+void PubConnect(zmq::context_t &ctx)
+{
+    zmq::socket_t pub(ctx, zmq::socket_type::pub);
+    pub.connect("ipc:///tmp/connect");
+    int i = 0;
+    while (true)
+    {
+        //  Write three messages, each with an envelope and content
+        // pub.send(zmq::str_buffer("Message A::"), zmq::send_flags::sndmore);
+        pub.send(zmq::buffer("Pub1!!!!!!!!! message::" + std::to_string(i)));
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        i++;
+    }
+}
 
 
 int main(int argc, char const *argv[])
 {
     zmq::context_t ctx;
     
-    auto pubthread = std::async(std::launch::async, [&ctx](){Pub(ctx);});
+    // auto pubthread = std::async(std::launch::async, [&ctx](){Pub(ctx);});
+    auto subthread = std::async(std::launch::async, [&ctx](){PubConnect(ctx);});
 
-    pubthread.wait();
-
+    // pubthread.wait();
+    subthread.wait();
     return 0;
 }
--- my_project/zeromq/b.cpp
+++ my_project/zeromq/b.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <array>
 
 #include "zmq.hpp"
 #include "zmq_addon.hpp"
@@ -12,20 +13,44 @@
 void Sub(zmq::context_t &ctx)
 {
     zmq::socket_t subscriber(ctx, zmq::socket_type::sub);
-    subscriber.connect("ipc://test_desu");
+    subscriber.connect("ipc:///tmp/hajime_quaternion");
     subscriber.set(zmq::sockopt::subscribe, "");
-
+    std::array<uint8_t,512> data;
+    zmq::mutable_buffer buf(data.data(),data.size());
     while (1)
     {
         // Receive all parts of the message
         std::vector<zmq::message_t> recv_msgs;
-        zmq::recv_result_t result =
-            zmq::recv_multipart(subscriber, std::back_inserter(recv_msgs));
-        assert(result && "recv failed");
-        assert(*result == 2);
+        zmq::recv_buffer_result_t result = subscriber.recv(buf);//,zmq::recv_flags::dontwait);
+        // assert(result && "recv failed"); // dame mitai
+        // assert(*result == 2); // dame mitai
+        std::cout << std::string(reinterpret_cast<char*>(buf.data())) << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        
+    }
+}
 
-        std::cout << "Thread2: [" << recv_msgs[0].to_string() << "] "
-                  << recv_msgs[1].to_string() << std::endl;
+void Subbind(zmq::context_t &ctx)
+{
+    zmq::socket_t subscriber(ctx, zmq::socket_type::sub);
+    subscriber.bind("ipc:///tmp/connect");
+    subscriber.set(zmq::sockopt::subscribe, "");
+    size_t buf_size = 256; //byte
+    std::array<uint8_t,buf_size> data;
+    // void * buf_data = reinterpret_cast<void*>(malloc(buf_size));
+    zmq::mutable_buffer buf(data.data(),data.size());
+    // zmq::mutable_buffer buf(buf_data,buf_size);
+    while (1)
+    {
+        // Receive all parts of the message
+        std::vector<zmq::message_t> recv_msgs;
+        zmq::recv_buffer_result_t result = subscriber.recv(buf);//,zmq::recv_flags::dontwait);
+        // assert(result && "recv failed"); // dame mitai
+        // assert(*result == 2); // dame mitai
+        std::cout << std::string(reinterpret_cast<char*>(buf.data())) << std::endl;
+        data.fill(0);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        
     }
 }
 
@@ -33,9 +58,11 @@ int main(int argc, char const *argv[])
 {
     zmq::context_t ctx;
     
-    auto subthread = std::async(std::launch::async, [&ctx](){Sub(ctx);});
+    // auto subthread = std::async(std::launch::async, [&ctx](){Sub(ctx);});
+    auto bindthread = std::async(std::launch::async, [&ctx](){Subbind(ctx);});
 
-    subthread.wait();
+    // subthread.wait();
+    bindthread.wait();
 
     return 0;
 }
