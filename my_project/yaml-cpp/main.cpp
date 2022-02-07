d3b2c5b68e3292334843efd735496a7c87bd8f45
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2022-02-07 23:51:23 +0900

    asdfojwaeoifjoiwejfiowjeofiwefowefioweowefoew

--- my_project/yaml-cpp/main.cpp
+++ my_project/yaml-cpp/main.cpp
@@ -1,13 +1,14 @@
 #include <string>
 #include <iostream>
-#include "yaml-cpp/yaml.h"
-#include "yaml-cpp/emitterstyle.h"
-#include "yaml-cpp/eventhandler.h"
+#include <yaml-cpp/yaml.h>
+#include <yaml-cpp/emitterstyle.h>
+#include <yaml-cpp/eventhandler.h>
 int main(){
-	YAML::Node config 	= YAML::LoadFile("controller.yaml");
-	int controller			= config["robot"]["controller"].as<int>();
-	int baudrate			= config["robot"]["baudrate"].as<int>();
-	std::string device 	= config["robot"]["device"].as<std::string>();
-	std::string robot   = config["model"].as<std::string>();
-	std::cout << robot << device << baudrate << std::endl;
+	std::string s("controller.yaml");
+	YAML::Node config 	= YAML::LoadFile(s);
+//	int controller			= config["robot"]["controller"].as<int>();
+//	int baudrate			= config["robot"]["baudrate"].as<int>();
+//	std::string device 	= config["robot"]["device"].as<std::string>();
+//	std::string robot   = config["model"].as<std::string>();
+//	std::cout << robot << device << baudrate << std::endl;
 }
