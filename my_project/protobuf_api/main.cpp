bbc2a62c393f36fb2e3490ebf50ab873443f2365
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2022-03-06 21:42:51 +0900

    asdfoijdasfoji

---
+++ my_project/protobuf_api/main.cpp
@@ -0,0 +1,14 @@
+#include "data.pb.h"
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cstdint>
+#include <google/protobuf/descriptor.h>
+
+
+int main(int argc, char const *argv[])
+{
+    test::data proto;
+    
+    return 0;
+}
