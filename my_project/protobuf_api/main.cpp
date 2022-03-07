20eebacbbb62fd26cce3b128f308413bfb1f7657
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2022-03-07 20:00:20 +0900

    asddfoijasdfioj

--- my_project/protobuf_api/main.cpp
+++ my_project/protobuf_api/main.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/message.h>
+#include <google/protobuf/text_format.h>
 
 
 int main(int argc, char const *argv[])
@@ -17,21 +18,23 @@ int main(int argc, char const *argv[])
     proto.set_cnt_i(283);
     proto.set_cnt_j(876);
     proto.mutable_pos2d()->set_pos_x(876);
-    proto.mutable_pos2dcf()->set_is_detect(true);
+    // proto.mutable_pos2dcf()->set_is_detect(true);
     const auto rflc = proto.GetReflection();
     std::vector<const google::protobuf::FieldDescriptor*> listfield;
     rflc->ListFields(proto,&listfield);
     for(const auto &field:listfield){
         std::cout << field->full_name() << "     " << field->name();
         if(field->type() == google::protobuf::FieldDescriptor::TYPE_MESSAGE){
-            const google::protobuf::Message &aaa =  rflc->GetMessage(proto,field,rflc->GetMessageFactory());
+            const auto &aaa =  google::protobuf::DynamicMessageFactory(field->containing_type())->New();
+            // const auto &aaa =  rflc->MutableMessage(&proto,field,rflc->GetMessageFactory());
+            std::cout << "message "  << aaa->x_vec();
         }
         else{ //fieldの場合
-            if(field->type() == google::protobuf::FieldDescriptor::TYPE_SINT32){
-                std::cout << " value::" << rflc->GetInt32(proto,field);
+            if(field->type() == google::protobuf::FieldDescriptor::TYPE_DOUBLE){
+                std::cout << " value::" << rflc->GetDouble(proto,field);
             }
         }
-
+        std::cout << std::endl;
     }
 
     return 0;
