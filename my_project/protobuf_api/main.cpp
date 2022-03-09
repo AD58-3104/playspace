9f6dbd5453dfe480a04684cfe0c67971daed2eb6
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2022-03-09 18:47:37 +0900

    asdfoijasdfoij

--- my_project/protobuf_api/main.cpp
+++ my_project/protobuf_api/main.cpp
@@ -5,49 +5,82 @@
 #include <utility>
 #include <cstdint>
 #include <variant>
-#include <boost/hana.hpp>
-#include <boost/hana/ext/std/tuple.hpp>
+#include <boost/asio.hpp>
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/message.h>
 #include <google/protobuf/text_format.h>
 
-using proto_tuple = std::tuple<test::data, test::Pos2DCf, test::Pos2D>;
-// test::data a;
-// test::Pos2DCf b;
-// test::Pos2D c;
+#define PROTOBUF_CLASS_LIST test::data, test::Pos2DCf, test::Pos2D
+using proto_tuple = std::tuple<PROTOBUF_CLASS_LIST>;
+using proto_variant = std::variant<PROTOBUF_CLASS_LIST>;
+#undef PROTOBUF_CLASS_LIST
 static const proto_tuple PROTO_LIST(test::data::default_instance(), test::Pos2DCf::default_instance(), test::Pos2D::default_instance());
-using proto_variant = std::variant<test::data, test::Pos2DCf, test::Pos2D>;
+
+
+
+/**
+ * @brief オーバーロード解決が面倒なのでこの中でポストした方が良いかも?
+ * でも寿命が相変わらず怪しい.コピーだから平気かな.コピーが多くなってるからここの一回だけでなんとかしたいね.
+ * 
+ */
+struct ProcessWork
+{
+    ProcessWork(boost::asio::io_context &io_ctx) : io_ctx_(io_ctx) {}
+    void operator()(test::data data)
+    {
+
+        io_ctx_.post(
+            [data = data]()
+            { std::cout << data.DebugString(); });
+    }
+    void operator()(test::Pos2D data)
+    {
+        if (data.has_pos_x())
+            std::cout << data.pos_x() << std::endl;
+    }
+    void operator()(test::Pos2DCf data)
+    {
+        if (data.has_is_detect())
+        {
+            std::cout << data.is_detect() << std::endl;
+        }
+    }
+
+private:
+    boost::asio::io_context &io_ctx_;
+};
 
 void createMatchedProto(google::protobuf::FieldDescriptor *field, const test::data message_instance, proto_variant &output)
 {
 }
 
-
-
 template <size_t N = 0>
-void iterate_proto_tuple(const proto_tuple &t,const google::protobuf::FieldDescriptor* field,google::protobuf::Message& msg_instance)
+void iterate_proto_tuple(const proto_tuple &t, const google::protobuf::FieldDescriptor *field, google::protobuf::Message &msg_instance, const ProcessWork &worker)
 {
     if constexpr (N < std::tuple_size<proto_tuple>::value)
     {
         const auto &reference_instance = std::get<N>(t);
         if (field->message_type() == reference_instance.GetDescriptor())
         {
-            auto* new_ins = reference_instance.New();
-            // new_ins = google::protobuf::DynamicCastToGenerated<decltype(new_ins)>(google::protobuf::MessageFactory::generated_factory()->GetPrototype(field->message_type())->New()); //無理.
-            // new_ins->CopyFrom(*(google::protobuf::MessageFactory::generated_factory()->GetPrototype(field->message_type())->New())); //可能なやつ.
+            auto *new_ins_ptr = reference_instance.New();
+            // new_ins_ptr = google::protobuf::DynamicCastToGenerated<decltype(new_ins_ptr)>(google::protobuf::MessageFactory::generated_factory()->GetPrototype(field->message_type())->New()); //無理.
+            // new_ins_ptr->CopyFrom(*(google::protobuf::MessageFactory::generated_factory()->GetPrototype(field->message_type())->New())); //可能なやつ.
             const auto rflc = msg_instance.GetReflection();
-            new_ins->CopyFrom(rflc->GetMessage(msg_instance,field,google::protobuf::MessageFactory::generated_factory()));
-            std::cout << "matched!!! "<< new_ins->DebugString() << std::endl;
+            new_ins_ptr->CopyFrom(rflc->GetMessage(msg_instance, field));
+            proto_variant new_instance(*new_ins_ptr);
+            // worker.io_ctx_.post(std::bind(std::visit, worker, new_instance)); //寿命がかなり怪しい
+            std::visit(worker,new_instance);
         }
-        else{
+        else
+        {
             std::cout << "else | given ::" << field->message_type()->full_name() << " tuple ::" << reference_instance.GetDescriptor()->full_name() << std::endl;
         }
-        iterate_proto_tuple<N + 1>(t, field,msg_instance);
+        iterate_proto_tuple<N + 1>(t, field, msg_instance, worker);
     }
 }
 
 template <class protobuf_type>
-void iterate_proto(protobuf_type prt)
+void iterate_proto_and_post(protobuf_type prt, const ProcessWork &worker)
 {
     const auto rflc = prt.GetReflection();
     std::vector<const google::protobuf::FieldDescriptor *> listfield;
@@ -57,17 +90,17 @@ void iterate_proto(protobuf_type prt)
         // std::cout << field->full_name() << "     " << field->name();
         if (field->type() == google::protobuf::FieldDescriptor::TYPE_MESSAGE)
         {
-            using namespace test;
+            // using namespace test;
             // const auto &aaa =  rflc->MutableMessage(&prt,field,rflc->GetMessageFactory());
-            auto func = [&field](const auto &message_instance)
-            {
-                if (field->message_type()->name() == message_instance.GetDescriptor()->name())
-                {
-                    auto new_ins = message_instance.New();
-                    new_ins = google::protobuf::MessageFactory::generated_factory()->GetPrototype(field->message_type())->New();
-                }
-            };
-            iterate_proto_tuple<0>(PROTO_LIST,field,prt);
+            // auto func = [&field](const auto &message_instance)
+            // {
+            //     if (field->message_type()->name() == message_instance.GetDescriptor()->name())
+            //     {
+            //         auto new_ins = message_instance.New();
+            //         new_ins = google::protobuf::MessageFactory::generated_factory()->GetPrototype(field->message_type())->New();
+            //     }
+            // };
+            iterate_proto_tuple<0>(PROTO_LIST, field, prt, worker);
             // std::cout << " message " << field->message_type()->name();
         }
         else
@@ -82,6 +115,8 @@ void iterate_proto(protobuf_type prt)
 
 int main(int argc, char const *argv[])
 {
+    boost::asio::io_context ioctx;
+    ProcessWork worker(ioctx);
     test::data proto;
     proto.set_str("aaaa");
     proto.set_x_vec(765);
@@ -91,28 +126,29 @@ int main(int argc, char const *argv[])
     proto.set_cnt_j(876);
     proto.mutable_pos2d()->set_pos_x(876);
     proto.mutable_pos2dcf()->set_is_detect(true);
-    // const auto rflc = proto.GetReflection();
-    // std::vector<const google::protobuf::FieldDescriptor *> listfield;
-    // rflc->ListFields(proto, &listfield);
-    // for (const auto &field : listfield)
-    // {
-    //     std::cout << field->full_name() << "     " << field->name();
-    //     if (field->type() == google::protobuf::FieldDescriptor::TYPE_MESSAGE)
-    //     {
-    //         // const auto &aaa =  google::protobuf::MessageFactory::generated_factory()->GetPrototype(field->message_type())->New();
-    //         // const auto &aaa =  rflc->MutableMessage(&proto,field,rflc->GetMessageFactory());
-    //         std::cout << " message " << field->message_type()->name();
-    //     }
-    //     else
-    //     { // fieldの場合
-    //         if (field->type() == google::protobuf::FieldDescriptor::TYPE_DOUBLE)
-    //         {
-    //             std::cout << " value::" << rflc->GetDouble(proto, field);
-    //         }
-    //     }
-    //     std::cout << std::endl;
-    // }
-    iterate_proto<test::data>(proto);
+    iterate_proto_and_post(proto, worker);
 
     return 0;
 }
+
+// const auto rflc = proto.GetReflection();
+// std::vector<const google::protobuf::FieldDescriptor *> listfield;
+// rflc->ListFields(proto, &listfield);
+// for (const auto &field : listfield)
+// {
+//     std::cout << field->full_name() << "     " << field->name();
+//     if (field->type() == google::protobuf::FieldDescriptor::TYPE_MESSAGE)
+//     {
+//         // const auto &aaa =  google::protobuf::MessageFactory::generated_factory()->GetPrototype(field->message_type())->New();
+//         // const auto &aaa =  rflc->MutableMessage(&proto,field,rflc->GetMessageFactory());
+//         std::cout << " message " << field->message_type()->name();
+//     }
+//     else
+//     { // fieldの場合
+//         if (field->type() == google::protobuf::FieldDescriptor::TYPE_DOUBLE)
+//         {
+//             std::cout << " value::" << rflc->GetDouble(proto, field);
+//         }
+//     }
+//     std::cout << std::endl;
+// }
