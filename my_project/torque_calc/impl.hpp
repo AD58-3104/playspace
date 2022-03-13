1396e39f385a8f1d5b110c478308d31384c6b7ea
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2022-03-14 01:21:48 +0900

    soif

--- my_project/torque_calc/impl.hpp
+++ my_project/torque_calc/impl.hpp
@@ -4,16 +4,22 @@
 #include <fstream>
 #include <optional>
 #include <vector>
+#include <memory>
 struct link{
-    link& parent_;
+    std::weak_ptr<link> parent_;
     using child_link = link;
-    std::optional<child_link&> roll_;
-    std::optional<child_link&> pitch_;
-    std::optional<child_link&> yaw_;
+    std::weak_ptr<child_link> roll_;
+    std::weak_ptr<child_link> pitch_;
+    std::weak_ptr<child_link> yaw_;
     double mass_;
     std::vector<double> inertia_;
-    link(link& parent,const double& mass,const std::vector<double>& inertia):parent_(parent),roll_(),pitch_(),yaw_(),mass_(mass),inertia_(inertia) {
-        static_assert("inertia must have three item",inertia.size() == 3);
+    link(std::shared_ptr<link> parent,const double& mass,const std::vector<double>& inertia):parent_(parent),roll_(),pitch_(),yaw_(),mass_(mass),inertia_(inertia) {
+        assert(inertia.size() == 3); //"inertia must have three item"
     }
+};
+
+class robot{
+    private:
+    std::vector<std::shared_ptr<link>> link_list_;
 };
 
---
+++ my_project/torque_calc/main.cpp
@@ -0,0 +1,7 @@
+#include "impl.hpp"
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
