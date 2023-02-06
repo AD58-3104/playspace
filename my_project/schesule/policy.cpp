9e90d25528e2a747d70428774ede72ee5892f426
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2023-02-07 00:50:39 +0900

    update policy

--- my_project/schesule/policy.cpp
+++ my_project/schesule/policy.cpp
@@ -3,13 +3,19 @@
 #include <unistd.h>
 #include <sched.h>
 #include <sys/types.h>
+#include <string.h>
+#include <errno.h>
 
 int main(int argc, char const *argv[])
 {
     auto pid = getpid();
     struct sched_param para;
     para.sched_priority = 5;
-    sched_setscheduler(pid,SCHED_BATCH,&para);
+    int err = sched_setscheduler(pid,SCHED_RR,&para);
+    if(err == -1){
+        std::cout << "error is " << strerror(errno) << std::endl;
+    }
+    sleep(10);
     auto policy = sched_getscheduler(pid);
     std::cout << policy << std::endl;
     return 0;
