d6fcccc6665631cc7dbff3f91dc8f59bfadbe142
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2023-03-10 17:05:31 +0900

    aaoisdjfoisdjf

--- a.cpp
+++ a.cpp
@@ -1,46 +1,7 @@
-/*
-CITBrainsのメンバーを管理するプログラム
-管理する情報は、メンバーの学籍番号、名前、在籍してるかどうか、学年
-*/
-
 #include <iostream>
-#include <bitset>
-#include <string>
-
-class menber_info
-{
-private:
-	std::string student_number = "19C1012";
-	std::string name = "inoue satoshi";
-
-public:
-	bool is_participated;
-	int grade;
-	void printData()
-	{
-		std::cout << menbers[i].student_number << menbers[i].name << std::endl;
-		return;
-	}
-};
-
-void printMenberData(menber_info info)
-{
-	std::cout << menbers[i].student_number << menbers[i].name << std::endl;
-	return;
-}
 
 int main()
 {
-	menber_info menbers[30];
-	menbers[0].grade = 4;			   // 4年
-	menbers[0].is_participated = true; //参加してる
-
-	//メンバー全員の情報をcoutとかで出力したい！
-	for (size_t i = 0; i < count; i++)
-	{
-		menbers[i].printData();
-		printMenberData(menbers[8]);
-	}
-
+	std::cout << "aaa" << std::endl;
 	return 0;
 }
